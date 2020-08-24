be8b118453e961fd6796a644424f4a13c5d3f95d
Manojkumar M <manojkumar.m8497@gmail.com>
2020-08-24 20:37:02 +0530

    Updates

---
+++ precache-manifest.1fb58446871cfb16e97f45e046e9208b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b935b2223a986fad52f395ff357d2d71",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "c3bfab711c0fd0dd609a",
+    "url": "/Netflix-Clone/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5e7153dda8166b8a4332",
+    "url": "/Netflix-Clone/static/css/main.6e77d6d5.chunk.css"
+  },
+  {
+    "revision": "c3bfab711c0fd0dd609a",
+    "url": "/Netflix-Clone/static/js/2.48800d5b.chunk.js"
+  },
+  {
+    "revision": "d837bdfa2a2c08de9ff5012f63aaaf8c",
+    "url": "/Netflix-Clone/static/js/2.48800d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7153dda8166b8a4332",
+    "url": "/Netflix-Clone/static/js/main.916b91dc.chunk.js"
+  },
+  {
+    "revision": "b2b8b24758047419b104",
+    "url": "/Netflix-Clone/static/js/runtime-main.f28e37b4.js"
+  }
+]);
